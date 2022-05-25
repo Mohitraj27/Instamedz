8f5e4e7718a5d012d82631d8228ee6c33c167da4
intel <Mohitraj27>
2022-05-25 17:45:54 +0530

    Started Making LogIn Page

--- app/src/main/java/com/example/instamedz/MainActivity.java
+++ app/src/main/java/com/example/instamedz/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.instamedz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.instamedz.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button GetStarted;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GetStarted = findViewById(R.id.Get_started);
+        GetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent LogInPage=new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(LogInPage);
+            }
+        });
     }
 }
