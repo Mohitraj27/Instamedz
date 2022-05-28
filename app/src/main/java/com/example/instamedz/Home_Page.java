9d11e6b6c09591b90b783ea6139ea3a53e21af31
intel <Mohitraj27>
2022-05-28 13:32:16 +0530

    Added Navigation Drawer in Home Page

---
+++ app/src/main/java/com/example/instamedz/Home_Page.java
@@ -0,0 +1,26 @@
+package com.example.instamedz;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class Home_Page extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+   ImageView onCLick_menu_bar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_activity_main);
+
+
+
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/instamedz/MainActivity.java
+++ app/src/main/java/com/example/instamedz/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.instamedz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.instamedz.ui.login.LoginActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private Button GetStarted;
     private TextView Already_Have_A_account;
+private Button Login_button;
+
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,13 +31,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(SigUpPage);
             }
         });
-        Already_Have_A_account=findViewById(R.id.Already_have_a_account_text);
+        Already_Have_A_account=findViewById(R.id.Home_Page_Default_button);
         Already_Have_A_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent LogInPage=new Intent(MainActivity.this,LoginActivity.class);
+                Intent LogInPage=new Intent(MainActivity.this,Home_Page.class);
                 startActivity(LogInPage);
             }
         });
+
+
     }
 }
