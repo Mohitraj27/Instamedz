9c241cca07c22f10251db5a7c1fd4938db2dd045
intel <Mohitraj27>
2022-05-31 16:19:54 +0530

    Integrated Whatsapp API  in each section (Heart Care, Health Care , Eye Care, Bussiness Care , Soul care)

--- app/src/main/java/com/example/instamedz/Home_Page.java
+++ app/src/main/java/com/example/instamedz/Home_Page.java
@@ -1,14 +1,22 @@
 package com.example.instamedz;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 public class Home_Page extends AppCompatActivity {
+      ImageView Whatsapp_icon_heart_care;
+      ImageView Whatsapp_icon_health_care;
+      ImageView Whatsapp_icon_eye_care;
+      ImageView Whatsapp_icon_bussiness_care;
+     ImageView Whatsapp_icon_Soul_care;
 
     DrawerLayout drawerLayout;
    ImageView onCLick_menu_bar;
@@ -17,10 +25,201 @@ public class Home_Page extends AppCompatActivity {
    ImageView Heart_care;
    ImageView Soul_care;
    ImageView Business_care;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_activity_main);
+
+       Whatsapp_icon_health_care=findViewById(R.id.whatsapp_icon_health_care);
+        Whatsapp_icon_heart_care =findViewById(R.id.whatsapp_icon_heart_care);
+        Whatsapp_icon_eye_care=findViewById(R.id.whatsapp_icon_eye_care);
+        Whatsapp_icon_Soul_care=findViewById(R.id.whatsapp_icon_soul_care);
+        Whatsapp_icon_bussiness_care=findViewById(R.id.whatsapp_icon_business_care);
+
+
+       final  String num= "+7415070882";
+        final        String text_heart_care ="I Just Clicked on Heart Care whatsapp icon";
+        final String text_health_care="I Just Clicked on Health Care whatsapp icon";
+        final String text_eye_care="I Just Clicked on Eye Care Whatsapp icon";
+        final String text_soul_care="I Just Clicked on Soul Care Whatsapp icon";
+       final String text_bussines_care="I Just Clicked on Bussiness Care Whatsapp icon";
+
+
+        Whatsapp_icon_Soul_care.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean installed=isAppInstalled("com.whatsapp");
+                if (installed)
+                {
+                    Intent intent=new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("http://api.whatsapp.com/send?phone="+num+"&text="+text_soul_care));
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(Home_Page.this,"Whatsapp is not installed!",Toast.LENGTH_SHORT);
+                }
+
+            }
+
+            private boolean isAppInstalled(String s) {
+                PackageManager packageManager=getPackageManager();
+                boolean is_installed;
+                try {
+                    packageManager.getPackageInfo(s,PackageManager.GET_ACTIVITIES);
+                    is_installed=true;
+                } catch (PackageManager.NameNotFoundException e) {
+                    is_installed=false;
+                    e.printStackTrace();
+                }
+                return  is_installed;
+            }
+        });
+
+
+
+        Whatsapp_icon_bussiness_care.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean installed=isAppInstalled("com.whatsapp");
+                if (installed)
+                {
+                    Intent intent=new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("http://api.whatsapp.com/send?phone="+num+"&text="+text_bussines_care));
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(Home_Page.this,"Whatsapp is not installed!",Toast.LENGTH_SHORT);
+                }
+
+            }
+
+            private boolean isAppInstalled(String s) {
+
+
+
+                PackageManager packageManager=getPackageManager();
+                boolean is_installed;
+                try {
+                    packageManager.getPackageInfo(s,PackageManager.GET_ACTIVITIES);
+                    is_installed=true;
+                } catch (PackageManager.NameNotFoundException e) {
+                    is_installed=false;
+                    e.printStackTrace();
+                }
+                return  is_installed;
+            }
+
+
+
+
+        });
+
+        Whatsapp_icon_eye_care.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean installed=isAppInstalled("com.whatsapp");
+                if (installed)
+                {
+                    Intent intent=new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("http://api.whatsapp.com/send?phone="+num+"&text="+text_eye_care));
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(Home_Page.this,"Whatsapp is not installed!",Toast.LENGTH_SHORT);
+                }
+
+
+            }
+
+            private boolean isAppInstalled(String s) {
+                PackageManager packageManager=getPackageManager();
+                boolean is_installed;
+                try {
+                    packageManager.getPackageInfo(s,PackageManager.GET_ACTIVITIES);
+                    is_installed=true;
+                } catch (PackageManager.NameNotFoundException e) {
+                    is_installed=false;
+                    e.printStackTrace();
+                }
+                return  is_installed;
+            }
+
+        });
+
+
+
+
+
+        Whatsapp_icon_health_care.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean installed=isAppInstalled("com.whatsapp");
+                if (installed)
+                {
+                    Intent intent=new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("http://api.whatsapp.com/send?phone="+num+"&text="+text_health_care));
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(Home_Page.this,"Whatsapp is not installed!",Toast.LENGTH_SHORT);
+                }
+            }
+
+            private boolean isAppInstalled(String s) {
+
+                PackageManager packageManager=getPackageManager();
+                boolean is_installed;
+                try {
+                    packageManager.getPackageInfo(s,PackageManager.GET_ACTIVITIES);
+                    is_installed=true;
+                } catch (PackageManager.NameNotFoundException e) {
+                    is_installed=false;
+                    e.printStackTrace();
+                }
+                return  is_installed;
+            }
+
+        });
+
+
+                 Whatsapp_icon_heart_care.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        boolean installed=isAppInstalled("com.whatsapp");
+                          if (installed)
+                          {
+                              Intent intent=new Intent(Intent.ACTION_VIEW);
+                              intent.setData(Uri.parse("http://api.whatsapp.com/send?phone="+num+"&text="+text_heart_care));
+                              startActivity(intent);
+                          }
+                          else
+                          {
+                              Toast.makeText(Home_Page.this,"Whatsapp is not installed!",Toast.LENGTH_SHORT);
+                          }
+                    }
+
+                    private boolean isAppInstalled(String s) {
+                        PackageManager packageManager=getPackageManager();
+                        boolean is_installed;
+                        try {
+                            packageManager.getPackageInfo(s,PackageManager.GET_ACTIVITIES);
+                            is_installed=true;
+                        } catch (PackageManager.NameNotFoundException e) {
+                            is_installed=false;
+                            e.printStackTrace();
+                        }
+                        return  is_installed;
+                    }
+                });
+
+
   Eye_Care=findViewById(R.id.Eye_Care);
   Eye_Care.setOnClickListener(new View.OnClickListener() {
       @Override
