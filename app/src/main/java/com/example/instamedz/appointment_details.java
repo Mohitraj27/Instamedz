d61e132cbe10bc01d7bbcd804c9b759bc17ed15e
m3rcury02 <gunal2002@gmail.com>
2022-06-17 23:22:54 +0530

    Registered appointment_details to Cloud FireStore

--- app/src/main/java/com/example/instamedz/appointment_details.java
+++ app/src/main/java/com/example/instamedz/appointment_details.java
@@ -4,15 +4,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class appointment_details extends AppCompatActivity  implements AdapterView.OnItemSelectedListener {
 
 
     private Object v;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Button submit;
+    private EditText editTextName, editTextDoA, editTextGender, editTextEmail, editTextPhNo;
+    String email = editTextEmail.getText().toString().trim();
+    String PTname = editTextName.getText().toString().trim();
+    String DoA=editTextDoA.getText().toString().trim();
+    String PhNo=editTextPhNo.getText().toString().trim();
 
     public void toastMsg(String msg) {
         Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
@@ -26,7 +44,7 @@ public class appointment_details extends AppCompatActivity  implements AdapterVi
     // create array of Strings
     // and store name of Services
     String[] courses = { "Dr. Instamedz", "Dr. Mohit Raj",
-            "Dr. Devina Patel", "Dr. Instamedz",
+            "Dr. Devina Patel", "Dr. Gunal",
             "Dr. Sharvesh " };
 
 
@@ -34,10 +52,6 @@ public class appointment_details extends AppCompatActivity  implements AdapterVi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment_details);
-
-
-
-
         // Take the instance of Spinner and
         // apply OnItemSelectedListener on it which
         // tells which item of spinner is clicked
@@ -61,6 +75,19 @@ public class appointment_details extends AppCompatActivity  implements AdapterVi
         // Set the ArrayAdapter (ad) data on the
         // Spinner which binds data to spinner
         spin.setAdapter(ad);
+        editTextName=(EditText)findViewById(R.id.Patient_Name);
+        editTextDoA=(EditText)findViewById(R.id.Appoint_DOA);
+        editTextEmail=(EditText)findViewById(R.id.Gmail_id);
+        editTextPhNo=(EditText)findViewById(R.id.Contact_No);
+
+        submit=(Button) findViewById(R.id.appointment_submission);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AppointUser();
+            }
+        });
+
     }
 
     @Override
@@ -72,6 +99,32 @@ public class appointment_details extends AppCompatActivity  implements AdapterVi
                 courses[position],
                 Toast.LENGTH_LONG)
                 .show();
+    }
+    public void AppointUser()
+    {
+
+
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("Patient Name", PTname);
+        user.put("Email", email);
+        user.put("Date Of Appointment", DoA);
+        user.put("Phone Number",PhNo);
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(appointment_details.this,"Appointment Registered!",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(appointment_details.this,"Errpr -"+e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     @Override
