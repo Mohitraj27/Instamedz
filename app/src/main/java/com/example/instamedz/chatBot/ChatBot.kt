d7f0fc6d30b707152509f79077893c4e689a51bd
Mohit Raj <raj716980@gmail.com>
2022-06-23 16:39:28 +0530

    Completed Eye Care Chatbot Ui

--- app/src/main/java/com/example/instamedz/chatBot/ChatBot.kt
+++ app/src/main/java/com/example/instamedz/chatBot/ChatBot.kt
@@ -8,5 +8,6 @@ class ChatBot : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_bot)
+
     }
 }
---
+++ app/src/test/java/com/example/instamedz/databinding/FragmentQuestionBindingImplTest.java
@@ -0,0 +1,7 @@
+package com.example.instamedz.databinding;
+
+import junit.framework.TestCase;
+
+public class FragmentQuestionBindingImplTest extends TestCase {
+
+}
