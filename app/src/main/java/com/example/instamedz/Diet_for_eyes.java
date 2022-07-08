a6d794781af6ccf138bf250d163d7c03a99082c4
Mohit Raj <raj716980@gmail.com>
2022-07-08 23:19:07 +0530

    Worked on Animation of Eye Care Diet Page and Eye Hygiene Pages .

--- app/src/main/java/com/example/instamedz/Diet_for_eyes.java
+++ app/src/main/java/com/example/instamedz/Diet_for_eyes.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class Diet_for_eyes extends Home_Page {
-
+    ViewFlipper flipper;
+    ImageView prev_Button, next_Button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +19,49 @@ public class Diet_for_eyes extends Home_Page {
         fab.hide();
         alreadyExecuted=true;
         ToastAlreadyExecuted=true;
+
+
+
+        flipper = findViewById(R.id.view_flipper);
+        prev_Button = findViewById(R.id.prev_button);
+        next_Button = findViewById(R.id.next_button);
+
+        prev_Button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        // It is used to set the in and out
+                        // animation of View flipper.
+                        flipper.setInAnimation(Diet_for_eyes.this,
+                                R.anim.slide_right);
+                        flipper.setOutAnimation(Diet_for_eyes.this,
+                                R.anim.slide_left);
+
+                        // It shows previous item.
+                        flipper.showPrevious();
+                    }
+                });
+
+        next_Button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        // It is used to set the in and out
+                        // animation of View flipper.
+                        flipper.setInAnimation(Diet_for_eyes.this,
+                                android.R.anim.slide_out_right);
+                        flipper.setOutAnimation(Diet_for_eyes.this,
+                                android.R.anim.slide_in_left);
+
+                        // It shows next item.
+                        flipper.showNext();
+                    }
+                });
+
+
+
+
     }
 }
--- app/src/main/java/com/example/instamedz/ui/login/ui/Eye_hygiene_diet.java
+++ app/src/main/java/com/example/instamedz/ui/login/ui/Eye_hygiene_diet.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import com.example.instamedz.Home_Page;
 import com.example.instamedz.R;
 
 public class Eye_hygiene_diet extends Home_Page {
-
+    ViewFlipper flipper;
+    ImageView prev_Button, next_Button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +22,49 @@ public class Eye_hygiene_diet extends Home_Page {
         fab.hide();
         alreadyExecuted=true;
         ToastAlreadyExecuted=true;
-           }
+
+
+
+
+        flipper = findViewById(R.id.view_flipper);
+        prev_Button = findViewById(R.id.prev_button);
+        next_Button = findViewById(R.id.next_button);
+
+        prev_Button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        // It is used to set the in and out
+                        // animation of View flipper.
+                        flipper.setInAnimation(Eye_hygiene_diet.this,
+                                R.anim.slide_right);
+                        flipper.setOutAnimation(Eye_hygiene_diet.this,
+                                R.anim.slide_left);
+
+                        // It shows previous item.
+                        flipper.showPrevious();
+                    }
+                });
+
+        next_Button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        // It is used to set the in and out
+                        // animation of View flipper.
+                        flipper.setInAnimation(Eye_hygiene_diet.this,
+                                android.R.anim.slide_out_right);
+                        flipper.setOutAnimation(Eye_hygiene_diet.this,
+                                android.R.anim.slide_in_left);
+
+                        // It shows next item.
+                        flipper.showNext();
+                    }
+                });
+
+
+
+    }
 }
