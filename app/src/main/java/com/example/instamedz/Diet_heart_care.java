49ae4dff10b389e7d0b5693d24ca47f9d6e8d23f
m3rcury02 <gunal2002@gmail.com>
2022-07-12 02:45:12 +0530

    created nutricare home, options, prograin, tips pages

---
+++ app/src/main/java/com/example/instamedz/Diet_heart_care.java
@@ -0,0 +1,14 @@
+package com.example.instamedz;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Diet_heart_care extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diet_heart_care);
+    }
+}
