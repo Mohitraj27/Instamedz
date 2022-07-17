84a93bb02c217117769e9bc8aac70939849f6a25
m3rcury02 <gunal2002@gmail.com>
2022-07-17 20:08:15 +0530

    Business Care cardview

--- app/src/main/java/com/example/instamedz/business_options_cards.java
+++ app/src/main/java/com/example/instamedz/business_options_cards.java
@@ -1,26 +1,21 @@
 package com.example.instamedz;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 
-public class Business_options extends Home_Page {
+public class business_options_cards extends Home_Page {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View contentView = inflater.inflate(R.layout.activity_business_options, null, false);
+        View contentView = inflater.inflate(R.layout.activity_business_options_cards, null, false);
         drawerLayout.addView(contentView, 0);
         fab.hide();
         alreadyExecuted=true;
         ToastAlreadyExecuted=true;
     }
 
-    public void OpenBusinessCards(View view) {
-        Intent About_us_Intent = new Intent(Business_options.this, business_options_cards.class);
-        startActivity(About_us_Intent);
-    }
 }
